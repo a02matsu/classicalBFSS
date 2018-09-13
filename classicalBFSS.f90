e2ae3cc8cc77575e186b99b153e1421c1917e7a6
松浦壮 <matsu@MatsuBookPro.local>
2018-09-13 15:38:12 +0900

    OUTPUTの書き出しをパラメータでコントロール。XMATの初期値をガウス分布に、VMATの初期値を0に。

--- classicalBFSS.f90
+++ classicalBFSS.f90
@@ -11,32 +11,41 @@ implicit none
 
 complex(kind(0d0)), allocatable :: Xmat(:,:,:)
 complex(kind(0d0)), allocatable :: Vmat(:,:,:)
-complex(kind(0d0)), allocatable :: Amat(:,:,:)
+complex(kind(0d0)), allocatable :: Fmat(:,:,:)
 
 integer :: i,j,m,n,k
 
 call set_parameters
-call set_initial(Xmat,Vmat,Amat)
-call calc_force(Amat,Xmat)
+call set_initial(Xmat,Vmat,Fmat)
+call calc_force(Fmat,Xmat)
 
-open(unit=Xmat_FILE, file=Xmat_FILE_NAME, status='replace', action='write')
-open(unit=Vmat_FILE, file=Vmat_FILE_NAME, status='replace', action='write')
-open(unit=Xmode_FILE, file=Xmode_FILE_NAME, status='replace', action='write')
-open(unit=Vmode_FILE, file=Vmode_FILE_NAME, status='replace', action='write')
+if( write_output == 0 ) then
+  open(unit=Xmat_FILE, file=Xmat_FILE_NAME, status='replace', action='write')
+  open(unit=Vmat_FILE, file=Vmat_FILE_NAME, status='replace', action='write')
+  open(unit=Fmat_FILE, file=Fmat_FILE_NAME, status='replace', action='write')
+  open(unit=Xmode_FILE, file=Xmode_FILE_NAME, status='replace', action='write')
+  open(unit=Vmode_FILE, file=Vmode_FILE_NAME, status='replace', action='write')
+  open(unit=Fmode_FILE, file=Fmode_FILE_NAME, status='replace', action='write')
+endif
 do k=0,Ntau-1
   time=time+deltaT
-  call time_evolution_LeapFrog(Xmat,Vmat,Amat)
-
-  call write_matrix(Xmat,Xmat_FILE)
-  call write_matrix(Vmat,Vmat_FILE)
-  call write_modes(Xmat,Xmode_FILE)
-  call write_modes(Vmat,Vmode_FILE)
-
+  call time_evolution_LeapFrog(Xmat,Vmat,Fmat)
+  if( write_output == 0 ) then 
+    call write_matrix(Xmat,Xmat_FILE)
+    call write_matrix(Vmat,Vmat_FILE)
+    call write_matrix(Fmat,Fmat_FILE)
+    call write_modes(Xmat,Xmode_FILE)
+    call write_modes(Fmat,Fmode_FILE)
+  endif
 enddo
-close(Xmat_FILE)
-close(Vmat_FILE)
-close(Xmode_FILE)
-close(Vmode_FILE)
+if( write_output == 0 ) then
+  close(Xmat_FILE)
+  close(Vmat_FILE)
+  close(Fmat_FILE)
+  close(Xmode_FILE)
+  close(Vmode_FILE)
+  close(Fmode_FILE)
+endif
 
 open(unit=Outconf_FILE, file=Outconf_FILE_NAME, status='replace', form='unformatted')
 write(Outconf_File) job_number
--- subroutines.f90
+++ subroutines.f90
@@ -17,6 +17,7 @@ read(PAR_FILE,*) MASS2
 
 read(PAR_FILE,*) job_number
 read(PAR_FILE,*) new_config
+read(PAR_FILE,*) write_output
 read(PAR_FILE,*) totalT
 read(PAR_FILE,*) deltaT
 
@@ -36,13 +37,13 @@ end subroutine set_parameters
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! Initial values
-subroutine set_initial(Xmat,Vmat,Amat)
+subroutine set_initial(Xmat,Vmat,Fmat)
 use global_parameters
 implicit none
 
 complex(kind(0d0)), allocatable :: Xmat(:,:,:)
 complex(kind(0d0)), allocatable :: Vmat(:,:,:)
-complex(kind(0d0)), allocatable :: Amat(:,:,:)
+complex(kind(0d0)), allocatable :: Fmat(:,:,:)
 integer :: n, i,j
 double precision :: r
 
@@ -59,7 +60,7 @@ call system('./make_directory.sh')
 ! set matrices
 allocate( Xmat(1:NMAT,1:NMAT,1:DIM) )
 allocate( Vmat(1:NMAT,1:NMAT,1:DIM) )
-allocate( Amat(1:NMAT,1:NMAT,1:DIM) )
+allocate( Fmat(1:NMAT,1:NMAT,1:DIM) )
 
 if( new_config == 1 ) then 
   job_number=0
@@ -73,8 +74,10 @@ if( new_config == 1 ) then
   enddo
   call random_seed(put=seed(:))
 
-  call random_number(rmat)
-  rmat = 2d0*rmat - 1d0
+  !call random_number(rmat)
+  !rmat = 2d0*rmat - 1d0
+
+  call BoxMuller(rmat)
 
   ! make rmat traceless
   do n=1,DIM
@@ -101,7 +104,8 @@ if( new_config == 1 ) then
   !!!!!!!!!!!!!!!!!
   !! TEMPORARY
   !! Vmat must satisry [X_M, V_M]=0 
-  Vmat=Xmat
+  !Vmat=Xmat
+  Vmat=(0d0,0d0)
 
 else
   if( job_number== 0 ) then 
@@ -126,20 +130,22 @@ endif
 write(Outconf_FILE_NAME,'("CONFIG/lastconfig_",i4.4)') job_number
 write(Xmat_FILE_NAME,'("OUTPUT/Xmat_",i4.4)') job_number
 write(Vmat_FILE_NAME,'("OUTPUT/Vmat_",i4.4)') job_number
+write(Fmat_FILE_NAME,'("OUTPUT/Fmat_",i4.4)') job_number
 write(Xmode_FILE_NAME,'("OUTPUT/Xmode_",i4.4)') job_number
 write(Vmode_FILE_NAME,'("OUTPUT/Vmode_",i4.4)') job_number
+write(Fmode_FILE_NAME,'("OUTPUT/Fmode_",i4.4)') job_number
 end subroutine set_initial
 
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! Calculate forces
-subroutine calc_force(Amat,Xmat)
+subroutine calc_force(Fmat,Xmat)
 use global_parameters
 use matrix_functions, only : Matrix_Commutator
 implicit none
 
-complex(kind(0d0)), intent(out) :: Amat(1:NMAT,1:NMAT,1:DIM)
+complex(kind(0d0)), intent(out) :: Fmat(1:NMAT,1:NMAT,1:DIM)
 complex(kind(0d0)), intent(in) :: Xmat(1:NMAT,1:NMAT,1:DIM)
 
 complex(kind(0d0)) tmpmat(1:NMAT,1:NMAT)
@@ -147,16 +153,16 @@ complex(kind(0d0)) tmpmat2(1:NMAT,1:NMAT)
 
 integer :: n,m,i,j
 
-Amat=(0d0,0d0)
+Fmat=(0d0,0d0)
 do m=1,DIM
   do n=1,DIM
     if ( n .ne. m ) then
       call Matrix_commutator(tmpmat,Xmat(:,:,m),Xmat(:,:,n))
       call Matrix_commutator(tmpmat2,Xmat(:,:,n),tmpmat)
-      Amat(:,:,m)=Amat(:,:,m)+tmpmat2
+      Fmat(:,:,m)=Fmat(:,:,m)+tmpmat2
     endif
   enddo
-  Amat(:,:,m)=Amat(:,:,m) - dcmplx( MASS2 )*Xmat(:,:,m)
+  Fmat(:,:,m)=Fmat(:,:,m) - dcmplx( MASS2 )*Xmat(:,:,m)
 enddo
 
 end subroutine calc_force
@@ -164,21 +170,21 @@ end subroutine calc_force
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! 2nd order time evolution
-subroutine time_evolution_LeapFrog(Xmat,Vmat,Amat)
+subroutine time_evolution_LeapFrog(Xmat,Vmat,Fmat)
 use global_parameters
 implicit none
 
 complex(kind(0d0)), intent(inout) :: Xmat(1:NMAT,1:NMAT,1:DIM)
 complex(kind(0d0)), intent(inout) :: Vmat(1:NMAT,1:NMAT,1:DIM)
-complex(kind(0d0)), intent(inout) :: Amat(1:NMAT,1:NMAT,1:DIM)
-complex(kind(0d0)) :: Amat2(1:NMAT,1:NMAT,1:DIM)
+complex(kind(0d0)), intent(inout) :: Fmat(1:NMAT,1:NMAT,1:DIM)
+complex(kind(0d0)) :: Fmat2(1:NMAT,1:NMAT,1:DIM)
 integer n
 
-Xmat = Xmat + deltaT*VMat + dcmplx(0.5d0*deltaT*deltaT)*Amat
+Xmat = Xmat + deltaT*VMat + dcmplx(0.5d0*deltaT*deltaT)*Fmat
 
-call calc_force(Amat2,Xmat)
-Vmat = Vmat + dcmplx(0.5d0*deltaT)*(Amat+Amat2)
-Amat=Amat2
+call calc_force(Fmat2,Xmat)
+Vmat = Vmat + dcmplx(0.5d0*deltaT)*(Fmat+Fmat2)
+Fmat=Fmat2
 
 end subroutine time_evolution_LeapFrog
 
@@ -258,6 +264,63 @@ enddo
 write(FILE_NUM,*)
 
 end subroutine write_modes
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! Gaussian random number
+!!  BoxMuller(gauss)
+!!
+!! generage ensemble exp(-1/2(x^2) and exp(-1/2(y^2))
+!! 
+!! output 2N gaussian randum numbers
+!! gauss is an allocatable array.
+!! It will be reallocated after calling this routine.
+subroutine BoxMuller(gauss)
+implicit none
+
+double precision, intent(out) :: gauss(:,:,:)
+double precision, parameter :: PI=dacos(-1d0)
+double precision, allocatable :: rand(:)
+double precision, allocatable :: tmp_gauss(:)
+
+integer :: K,L,M,N
+integer i,a,b,c
+
+K=size(gauss,1)
+L=size(gauss,2)
+M=size(gauss,3)
+
+N=K*L*M
+if( mod(N,2) == 0 ) then
+  allocate( rand(1:N) )
+  allocate( tmp_gauss(1:N) )
+else
+  allocate( rand(1:N+1) )
+  allocate( tmp_gauss(1:N+1) )
+endif
+
+call random_number( rand )
+!call genrand_real3(rand)
+!write(*,*) rand
+
+do i=1,N/2
+  tmp_gauss(2*i-1) = dsqrt(-2d0*dlog(rand(2*i-1)))*dsin(2d0*Pi*rand(2*i))
+  tmp_gauss(2*i) = dsqrt(-2d0*dlog(rand(2*i-1)))*dcos(2d0*Pi*rand(2*i))
+enddo
+if( mod(N,2) /= 0 ) then
+  tmp_gauss(N) = dsqrt(-2d0*dlog(rand(N)))*dcos(2d0*Pi*rand(N+1))
+endif
+
+do c=1,M
+  do b=1,L
+    do a=1,K
+      i = K*L*(c-1) + K*(b-1) + a
+      gauss(a,b,c) = tmp_gauss(i)
+    enddo
+  enddo
+enddo
+
+end subroutine BoxMuller
+
 
 
 end module subroutines
